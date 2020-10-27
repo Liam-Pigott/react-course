a56c7be9a10768752ffafe3db176477c9d2e689c
Liam Pigott <lpigot92@gmail.com>
2020-10-27 09:35:49 +0000

    finishing useState coverage

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+	//Can always split into separate state values (name, age, message) and have dedicated functions to control state
+	const [person, setPerson] = useState({ name: 'Liam', age: 28, message: 'random message' });
+
+	const changeMessage = () => {
+		// setPerson(11); //valid but would reset the entire onject
+		// setPerson({ message: 'Hello World' }); //would reset the object to just message
+		setPerson({ ...person, message: 'Hello World' });
+	};
+	return (
+		<>
+			<h3>{person.name}</h3>
+			<h3>{person.age}</h3>
+			<h3>{person.message}</h3>
+			<button className='btn' onClick={changeMessage}>
+				Change Message
+			</button>
+		</>
+	);
 };
 
 export default UseStateObject;
