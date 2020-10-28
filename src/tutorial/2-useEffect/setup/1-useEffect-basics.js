e71b54737b8e742778b869eefcbfa010f653dbab
Liam Pigott <lpigot92@gmail.com>
2020-10-28 09:54:56 +0000

    covering useEffect applications

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,30 @@
 import React, { useState, useEffect } from 'react';
-// by default runs after every re-render
-// cleanup function
-// second parameter
+
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+	const [value, setValue] = useState(0);
+
+	// if(value > 0) { ...can't call hooks conditionally
+	useEffect(() => {
+		console.log('call useEffect');
+		// conditionals need to be used inside the hook
+		if (value > 0) {
+			document.title = `New Messages(${value})`;
+		}
+	}, [value]); // optional array of dependencies. [] will only run initial render
+
+	useEffect(() => {
+		console.log('Run initial render');
+	}, []);
+
+	console.log('Render component');
+	return (
+		<>
+			<h1>{value}</h1>
+			<button className='btn' onClick={() => setValue(value + 1)}>
+				Click me
+			</button>
+		</>
+	);
 };
 
 export default UseEffectBasics;
--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
-// cleanup function
-// second argument
-
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+	const [size, setSize] = useState(window.innerWidth);
+
+	const checkSize = () => {
+		setSize(window.innerWidth);
+	};
+
+	useEffect(() => {
+		console.log('use effect');
+		window.addEventListener('resize', checkSize);
+		return () => {
+			console.log('cleanup');
+			window.removeEventListener('resize', checkSize);
+		};
+	}); // could also solve issue by passing [] dependency in this case
+	console.log('render');
+	return (
+		<>
+			<h1>Window</h1>
+			<h2>{size} px</h2>
+		</>
+	);
 };
 
 export default UseEffectCleanup;
