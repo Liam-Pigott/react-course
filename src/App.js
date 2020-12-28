c82d063d514e597fc2b369aa160d326e3afb7ac6
Liam Pigott <lpigot92@gmail.com>
2020-12-28 23:05:21 +0000

    setup useRef and useReducer examples

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Setup from './tutorial/4-forms/setup/2-multiple-inputs';
-// import Final from './tutorial/2-useEffect/final/3-useEffect-fetch-data';
+import Setup from './tutorial/6-useReducer/setup';
+// import Final from './tutorial/6-useReducer/final';
 
 function App() {
 	return (
